197a84ca56445f7e89e917a1ba38cfa550b7232c
riakizo <riakmark02@gmail.com>
2023-02-17 05:09:12 -0800

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main = main function
- *
- * Return: 0
+ * main - entry point,
+ * printf to display the message
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
