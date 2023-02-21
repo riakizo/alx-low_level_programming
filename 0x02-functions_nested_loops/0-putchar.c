9fc4ebc602f1351642ef2e30a7221e65acd72946
riakzio <riakmark02@gmail.com>
2023-02-21 12:26:59 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * main - Entry point
- * Description: prints 'Holberton \n'
+ * Description: prints 'main \n'
  * Return: Always 0 (success)
  */
 int main(void)
 {
-char str[] = "Holberton\n";
+char str[] = "main\n";
 int i = 0;
 
 while (str[i] != '\0')
