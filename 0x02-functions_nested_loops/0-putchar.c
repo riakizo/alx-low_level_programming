32c636cd5f9892dde0df9f5c5fb01f25e524ef6a
riakzio <riakmark02@gmail.com>
2023-02-21 13:09:03 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
+
 /**
  * main - Entry point
- * Description: prints 'main \n'
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
-char str[] = "main\n";
-int i = 0;
-
-while (str[i] != '\0')
-	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
-	}
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
