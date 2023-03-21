9aa6348c7aa3670a6e77abcd06e8b528e22e5c31
riakizo <riakmark02@gmail.com>
2023-03-21 03:59:15 +0300

    function macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+#define ABS(x) (((x) < 0) ? (-(x)) : (x))
 #endif
 
