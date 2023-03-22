d10278a92e80b14e54d2e6a7a85c540368dab3cc
riakizo <riakmark02@gmail.com>
2023-03-22 22:30:52 +0300

    riak function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 
-
 /**
  *print_name - prints a name
  *@name: name to print
@@ -9,8 +8,7 @@
  *Return: void
  */
 void print_name(char *name, void (*f)(char *))
-{ 
+{
 	if (name != NULL && f != NULL)
-
 		f(name);
 }
