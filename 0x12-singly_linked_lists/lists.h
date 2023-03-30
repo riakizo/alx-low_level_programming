45ff45cac381670cdf3103f3257fc6014f3c4237
riakizo <riakmark02@gmail.com>
2023-03-31 00:41:31 +0300

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,12 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+void myStartupFun(void)__attribute__((constructor));
 
 /**
  * struct list_s - singly linked list
@@ -9,11 +16,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -22,4 +30,8 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+
+
+
 #endif
+
